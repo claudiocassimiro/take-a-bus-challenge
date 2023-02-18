89e51a5b66fde55a0f3909021ac9624bfabc5a7c
claudiocassimiro <days_surfing@live.com>
2023-02-18 18:56:00 -0300

    feat: pass new component to render on app

--- App.tsx
+++ App.tsx
@@ -1,64 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Geolocation from "react-native-geolocation-service";
-import xml2js from "xml2js";
-
-type Bbox = {
-  left: number;
-  down: number;
-  right: number;
-  up: number;
-};
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import Map from "./components/Map";
 
 export default function App() {
-  const [bBox, setBbox] = useState({} as Bbox);
-
-  useEffect(() => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        const leftAndRightToCalcBbox = 0.1;
-        const downAndUpToCalcBbox = 0.05;
-
-        setBbox({
-          left: longitude - leftAndRightToCalcBbox,
-          down: latitude - downAndUpToCalcBbox,
-          right: longitude + leftAndRightToCalcBbox,
-          up: latitude + downAndUpToCalcBbox,
-        });
-      },
-      (error) => {
-        console.log(error.code, error.message);
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  }, []);
-
-  useEffect(() => {
-    getNearbyBusStops();
-  }, [bBox]);
-
-  const getNearbyBusStops = async () => {
-    const { left, down, right, up } = bBox;
-    const response = await fetch(
-      `https://api.openstreetmap.org/api/0.6/map?bbox=${left},${down},${right},${up}&amenity=bus_stop`
-    );
-
-    const xmlString = await response.text();
-    const busStopConrdinates = xml2js.parseString(xmlString, (err, result) => {
-      try {
-        console.log(result);
-      } catch (error) {
-        console.log(error);
-      }
-    });
-  };
+  const [showBusStops, setShowBusStops] = useState(true);
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Map showBusStops={showBusStops} />
+      <TouchableOpacity
+        style={styles.hiddenMarkButton}
+        onPress={() => setShowBusStops(!showBusStops)}
+      >
+        <Text>{`${
+          showBusStops ? "Esconder" : "Ver"
+        } paradas de ônibus proximas`}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -69,5 +28,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    position: "relative",
+  },
+  hiddenMarkButton: {
+    position: "absolute",
+    bottom: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "white",
+    border: "none",
+    "border-radius": 5,
   },
 });
